052ae765ac7c369c8f9f9dca06974f8e60b09048
Hitesh Jangid <hiteshjangid@users.noreply.github.com>
2023-05-12 13:32:31 +0530

    Update orbs.php

--- orbs.php
+++ orbs.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 require 'db.php';
-$result = $db->query('SELECT o.id as orb_id, name,inet_ntoa(ip),ip, disabled, water_uuid, elec_uuid, elec_rvid, water_rvid, o.last_connectioned_on, r1.relative_value as elec_rv, r2.relative_value as water_rv, testing FROM orbs o LEFT JOIN relative_values r1 ON r1.id = o.elec_rvid LEFT JOIN relative_values r2 ON r2.id = o.water_rvid WHERE o.disabled = 0 ORDER BY `name`');
+$result = $db->query('SELECT name,inet_ntoa(ip) as ip_address,ip, disabled, water_uuid, elec_uuid, elec_rvid, water_rvid, o.last_connectioned_on, r1.relative_value as elec_rv, r2.relative_value as water_rv, testing FROM orbs o LEFT JOIN relative_values r1 ON r1.id = o.elec_rvid LEFT JOIN relative_values r2 ON r2.id = o.water_rvid WHERE o.disabled = 0 ORDER BY `name`');
 foreach ($result as $row) {
 	if ($row['disabled'] === '0') {
     if ($row['elec_rv'] == null) {
@@ -24,15 +24,15 @@ foreach ($result as $row) {
     }
     $msg = "/E{$elec}W{$water}&";
     // $msg = "?????";
-    exec('bash -c "exec nohup setsid echo \''.$msg.'\' | timeout 15s netcat '.$row['ip'].' 9950 > \''.$row['ip'].'\' 2>&1 &"');
+    exec('bash -c "exec nohup setsid echo \''.$msg.'\' | timeout 15s netcat '.$row['ip_address'].' 9950 > \''.$row['ip_address'].'\' 2>&1 &"');
     /* update last sent relative value to orb */
     $last_sent_relative_value = "$elec#$water";
     $stmt = $db->prepare('UPDATE orbs SET last_sent_relative_value = ? WHERE id = ?');
-    $stmt->execute(array($last_sent_relative_value, $row['orb_id']));
+    $stmt->execute(array($last_sent_relative_value, $row['id']));
     // $stmt = $db->prepare('UPDATE orbs SET last_resp = ? WHERE id = ?');
-    // $stmt->execute(array($result, $row['orb_id']));
+    // $stmt->execute(array($result, $row['id']));
 	} //else {
-	// 	shell_exec("echo \"^R00B00G00f32+\" | timeout 2s netcat {$row['orb_id']} 9950");
+	// 	shell_exec("echo \"^R00B00G00f32+\" | timeout 2s netcat {$row['ip_address']} 9950");
 	// }
 }
 ?>
