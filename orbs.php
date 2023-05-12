fbba85bb8772c07d8be6665278cc9b927952c225
Hitesh Jangid <hiteshjangid19@gmail.com>
2023-05-12 12:11:55 +0530

    use join to avoid extra query for relative value

--- orbs.php
+++ orbs.php
@@ -3,25 +3,32 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 require 'db.php';
-foreach ($db->query('SELECT id, elec_uuid, water_uuid, elec_rvid, water_rvid, disabled, INET_NTOA(ip) AS ip FROM orbs ORDER BY id ASC') as $row) {
+$result = $db->query('SELECT name,inet_ntoa(ip),ip, water_uuid, elec_uuid, elec_rvid, water_rvid, o.last_connectioned_on, r1.relative_value as elec_rv, r2.relative_value as water_rv, testing FROM orbs o LEFT JOIN relative_values r1 ON r1.id = o.elec_rvid LEFT JOIN relative_values r2 ON r2.id = o.water_rvid WHERE o.disabled = 0 ORDER BY `name`');
+foreach ($result as $row) {
 	if ($row['disabled'] === '0') {
-    if ($row['elec_uuid'] == null) {
+    if ($row['elec_rv'] == null) {
       $elec = 'X';
     } else {
-      $stmt = $db->prepare('SELECT relative_value FROM relative_values WHERE id = ?');
-      $stmt->execute(array($row['elec_rvid']));
-      $elec = round(($stmt->fetchColumn() / 100) * 4); // must be integer 0-4
+      // $stmt = $db->prepare('SELECT relative_value FROM relative_values WHERE id = ?');
+      // $stmt->execute(array($row['elec_rvid']));
+      // $elec = round(($stmt->fetchColumn() / 100) * 4); // must be integer 0-4
+      $elec = round($row['elec_rv'] / 100) * 4; // must be integer 0-4
     }
-    if ($row['water_uuid'] == null) {
+    if ($row['water_rv'] == null) {
       $water = 'X';
     } else {
-      $stmt = $db->prepare('SELECT relative_value FROM relative_values WHERE id = ?');
-      $stmt->execute(array($row['water_rvid']));
-      $water = round(($stmt->fetchColumn() / 100) * 4); // must be integer 0-4
+      $elec = round($row['water_rv'] / 100) * 4; // must be integer 0-4
+      // $stmt = $db->prepare('SELECT relative_value FROM relative_values WHERE id = ?');
+      // $stmt->execute(array($row['water_rvid']));
+      // $water = round(($stmt->fetchColumn() / 100) * 4); // must be integer 0-4
     }
     $msg = "/E{$elec}W{$water}&";
     // $msg = "?????";
     exec('bash -c "exec nohup setsid echo \''.$msg.'\' | timeout 15s netcat '.$row['ip'].' 9950 > \''.$row['ip'].'\' 2>&1 &"');
+    /* update last sent relative value to orb */
+    $last_sent_relative_value = "$elec#$water";
+    $stmt = $db->prepare('UPDATE orbs SET last_sent_relative_value = ? WHERE id = ?');
+    $stmt->execute(array($last_sent_relative_value, $row['id']));
     // $stmt = $db->prepare('UPDATE orbs SET last_resp = ? WHERE id = ?');
     // $stmt->execute(array($result, $row['id']));
 	} //else {
